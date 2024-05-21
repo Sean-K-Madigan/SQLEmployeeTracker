909ff7fe21d44e2cc734e2906a34e81101717482
Sean-K-Madigan <157637384+Sean-K-Madigan@users.noreply.github.com>
2024-05-20 22:07:31 -0600

    update seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,25 @@
+INSERT INTO department (department_name)
+VALUES ('Engineering'),
+       ('Finance'),
+       ('Legal'),
+       ('Sales');
+
+INSERT INTO roles (title, salary, department_id)
+VALUES ('Software Engineer', 120000, 1),
+       ('Lead Engineer', 150000, 1),
+       ('Accountant', 125000, 2),
+       ('Account Manager', 160000, 2),
+       ('Lawyer', 190000, 3),
+       ('Legal Team Lead', 250000, 3),
+       ('Salesperson', 80000, 4),
+       ('Sales Manager', 120000, 4);
+
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES ('John', 'Doe', 8, null),
+       ('Mike', 'Chan', 7, ),
+       ('Ashley', 'Rodriguez', 2, null),
+       ('Kevin', 'Tupik', 1, ),
+       ('Kunal', 'Singh', 4, null),
+       ('Malia', 'Brown', 3, ),
+       ('Sarah', 'Lourd', 6, null),
+       ('Tom', 'Allen', 5, ),
