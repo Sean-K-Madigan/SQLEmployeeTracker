b6b0595681ccdb5c0be50a2ac424558f52eda46b
Sean-K-Madigan <157637384+Sean-K-Madigan@users.noreply.github.com>
2024-05-23 16:09:55 -0600

    add queries and work on completing the inquirer file

--- db/schema.sql
+++ db/schema.sql
@@ -21,7 +21,6 @@ CREATE TABLE employees (
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   role_id INT NOT NULL,
-  -- Maybe make a mananager boolean ?
   manager_id INT,
   FOREIGN KEY (role_id) REFERENCES roles (id)
 );
--- server.js
+++ server.js
@@ -10,9 +10,20 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Connect to database
+const pool = new Pool(
+  {
+    user: 'postgres',
+    password: 'password',
+    host: 'localhost',
+    database: 'employee_db'
+  },
+  console.log(`Connected to the employee_db database.`)
+)
+
 pool.connect();
 
-
+// complete the rest of file
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
